dce0a37670c498307792fb5ba2681e7f63be7265
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-01-20 19:54:30 +0900

    データベースをjsonファイルからpostgresへと変更

--- README.md
+++ README.md
@@ -4,13 +4,27 @@
 
 
 ## 何ができるものなのか
-ローカルのjsonファイルにメモ（id,title,content)を保存、読み込み削除ができるアプリです。
+postgresにメモ（id,title,content)を保存、読み込み削除ができるアプリです。
 
 ## 構築やインストール方法
-
+### Rubyの環境構築
 ```
 % git clone https://github.com/kazuyasai10/sinatara-basic.git
 % bundle install --path vendor/bundle
 % bundle exec ruby app.rb
 => http://localhost:4567
+```
+
+### postgresの環境構築
+
+```
+ CREATE DATABASE memo;
+```
+```
+CREATE TABLE memo_table
+ (
+   id VARCHAR(128) NOT NULL,
+   title VARCHAR(128) ,
+   content text,
+   PRIMARY KEY (id));
 ```
--- model.rb
+++ model.rb
@@ -6,9 +6,8 @@ require 'pg'
 
 class Memo
   JSON_FILE_PATH = 'data.json'
-CONNECTION = connection = PG::Connection.new(:dbname => 'memo')
-puts 'Successfully created connection to database.'
-p 
+  CONNECTION = PG::Connection.new(dbname: 'memo')
+
   attr_reader :id, :title, :content
 
   def initialize(id: '000', title: 'notitle', content: 'no-content')
@@ -19,8 +18,9 @@ p
 
   def self.find(id)
     memos = []
-    load_json['memos'].each do |memo|
-      memos << memo if memo['id'] == id
+    memo_tables = CONNECTION.exec("SELECT * FROM memo_table WHERE id= '#{id}';")
+    memo_tables.each do |memo|
+      memos << memo
     end
     memos[0]
     Memo.new(id: memos[0]['id'], title: memos[0]['title'], content: memos[0]['content'])
@@ -28,8 +28,8 @@ p
 
   def self.all
     memos = []
-    memo_table=CONNECTION.exec('SELECT * FROM memo_table;')
-    memo_table.each do |memo|
+    memo_tables = CONNECTION.exec('SELECT * FROM memo_table;')
+    memo_tables.each do |memo|
       memos << Memo.new(id: memo['id'], title: memo['title'], content: memo['content'])
     end
     memos
@@ -37,41 +37,18 @@ p
 
   def self.create(title: '', content: '')
     id = SecureRandom.uuid
-    json_data = load_json
-    json_data['memos'] << { 'id' => id, 'title' => title, 'content' => content }
-    save_json(json_data)
+    CONNECTION.exec("INSERT INTO memo_table VALUES('#{id}', '#{title}', '#{content}')")
   end
 
   def self.update(id, title: '', content: '')
-    json_data = load_json
-    json_data['memos'] = load_json['memos'].each do |memo|
-      if memo['id'] == id
-        memo['title'] = title
-        memo['content'] = content
-      end
-    end
-    save_json(json_data)
+    query = "UPDATE memo_table 
+    SET title = '#{title}',
+    content = '#{content}'
+    WHERE id = '#{id}'"
+    CONNECTION.exec(query)
   end
 
   def self.delete(id)
-    json_data = load_json
-    json_data['memos'].delete_if do |memo_hash|
-      memo_hash['id'] == id
-    end
-    save_json(json_data)
-  end
-
-  def self.load_json
-    File.open(JSON_FILE_PATH) do |f|
-      JSON.parse(f.read)
-    end
-  end
-
-  def self.save_json(json_data)
-    File.open(JSON_FILE_PATH, 'w') do |io|
-      JSON.dump(json_data, io)
-    end
+    CONNECTION.exec("DELETE FROM memo_table WHERE id = '#{id}'")
   end
 end
-
-p Memo.all
